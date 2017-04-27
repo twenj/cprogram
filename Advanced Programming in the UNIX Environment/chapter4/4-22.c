9f07adea4ffa2845ea626c4536894142cbdcfdbb
twenj <twenj1992@163.com>
2017-04-27 14:11:56 +0800

    #

--- Advanced Programming in the UNIX Environment/chapter4/4-22.c
+++ Advanced Programming in the UNIX Environment/chapter4/4-22.c
@@ -16,10 +16,10 @@ int main(int argc, char *argv[])
 		err_quit("usage: ftw <starting-pathname>");
 	ret = myftw(argv[1], myfunc);
 	
-	noto = nreg + ndir + nblk + nchr + nfifo + nslink + nsock;
+	ntot = nreg + ndir + nblk + nchr + nfifo + nslink + nsock;
 	
-	if (noto == 0)
-		noto = 1;
+	if (ntot == 0)
+		ntot = 1;
 	
 	printf("regular files = %7ld, %5.2f %%\n", nreg, nreg * 100.0/ntot);
 	printf("directories = %7ld, %5.2f %%\n", ndir, ndir * 100.0/ntot);
@@ -46,7 +46,7 @@ static int myftw(char *pathname, Myfunc *func)
 	
 	if (pathlen <= strlen(pathname)) {
 		pathlen = strlen(pathname) * 2;
-		if ((fullpath = realloc(fullpath, pathlen)) = NULL)
+		if ((fullpath = realloc(fullpath, pathlen)) == NULL)
 			err_sys("realloc faild");
 	}
 	
@@ -121,7 +121,53 @@ static int myfunc(const char *pathname, const struct stat *statptr, int type)
 	return(0);
 }
 
+#ifdef PATH_MAX
+static long pathmax = PATH_MAX;
+#else
+static long pathmax = 0;
+#endif
 
+static long posix_version = 0;
+static long xsi_version = 0;
+
+#define PATH_MAX_GUESS 1024
+
+char *path_alloc(size_t *sizep)
+{
+	char *ptr;
+	size_t size;
+	
+	if (posix_version == 0)
+		posix_version = sysconf(_SC_VERSION);
+	
+	if (xsi_version == 0) 
+		xsi_version = sysconf(_SC_XOPEN_VERSION);
+	
+	if (pathmax == 0) {
+		errno = 0;
+		if ((pathmax = pathconf("/", _PC_PATH_MAX)) < 0) {
+			if (errno == 0)
+				pathmax = PATH_MAX_GUESS;
+			else
+				err_sys("pathconf error for _PC_PATH_MAX");
+		} else {
+			pathmax++;
+		}
+	}
+	
+	if ((posix_version < 200112L) && (xsi_version < 4))
+		size = pathmax + 1;
+	else
+		size = pathmax;
+	
+	if ((ptr = malloc(size)) == NULL)
+		err_sys("malloc error for pathname");
+	
+	if (sizep != NULL)
+		*sizep = size;
+	
+	return(ptr);
+}
 
 
 
