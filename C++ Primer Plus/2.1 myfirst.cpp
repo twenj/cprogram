c959afe5a5dda810fdcf41a6b0b2e2c2b97cf780
twenj <twenj1992@163.com>
2017-12-05 11:20:29 +0800

    #

---
+++ C++ Primer Plus/2.1 myfirst.cpp
@@ -0,0 +1,10 @@
+// myfirst.cpp -- display a message
+#include <iostream>         // a PREPROCESSOR directive
+int main()                 // function header
+{                          // start of function body
+    using namespace std; // make definition body
+    cout << "Come up and C++ me some time.";    // message
+    cout << endl;
+    cout << "You won't regret it!" << endl;     // more output
+    return 0;       // terminate main()
+}                          // end of function body
