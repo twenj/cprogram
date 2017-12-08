4338eba6b9bfef442e7bb23a37da0381d946723a
twenj <twenj1992@163.com>
2017-12-08 17:35:11 +0800

    #

---
+++ C++ Primer Plus/11.3 usetime0.cpp
@@ -0,0 +1,29 @@
+// usetime0.cpp -- using the first draft of the Time class
+// compiled usetime0.cpp and mytime0.cpp together
+#include <iostream>
+#include "mytime0.h"
+
+int main()
+{
+    using std::cout;
+    using std::endl;
+    Time planning;
+    Time coding(2, 40);
+    Time fixing(5, 55);
+    Time total;
+
+    cout  << "planning time = ";
+    planning.Show();
+    cout << endl;
+
+    cout << "fixing time = ";
+    fixing.Show();
+    cout << endl;
+
+    total = coding.Sum(fixing);
+    cout << "coding.Sum(fixing) = ";
+    total.Show();
+    cout << endl;
+
+    return 0;
+}
